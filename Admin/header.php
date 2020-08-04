d2a92ce6cc17774dbed3e71d8fc483deacc458e4
veysel gunes <veysel.gunes36@gmail.com>
2020-08-04 17:08:25 +0300

    Session İşlemi Yapıldı

--- Admin/header.php
+++ Admin/header.php
@@ -19,7 +19,7 @@ $sonuc = $sorgu->fetch(PDO::PARAM_INT);
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <!-- FONTAWESOME STYLES-->
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
-       <!--CUSTOM BASIC STYLES-->
+    <!--CUSTOM BASIC STYLES-->
     <link href="assets/css/basic.css" rel="stylesheet" />
     <!--CUSTOM MAIN STYLES-->
     <link href="assets/css/custom.css" rel="stylesheet" />
--- Admin/index.php
+++ Admin/index.php
@@ -1,23 +1,30 @@
-<?php include 'header.php' ?>
-<?php include'Sliderbar.php' ?>
+<?php include 'header.php'; 
+ include'Sliderbar.php';
+if(!isset($_SESSION['kadi'])){
+    header('location:Login.php');
+}
 
- <div id="page-wrapper">
-            <div id="page-inner">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h1 class="page-head-line">ADMİN PANELİNE HOŞ GELDİNİZ</h1>
-                        <h1 class="page-subhead-line">Bütün işlemlerinizi bu panel üzerinden yapabilirsiniz </h1>
 
-                    </div>
-                </div>
-                <!-- /. ROW  -->
-               
+
+  ?>
+
+<div id="page-wrapper">
+    <div id="page-inner">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="page-head-line">ADMİN PANELİNE HOŞ GELDİNİZ</h1>
+                <h1 class="page-subhead-line">Bütün işlemlerinizi bu panel üzerinden yapabilirsiniz </h1>
+
             </div>
-            <!-- /. PAGE INNER  -->
         </div>
-      
-     
+        <!-- /. ROW  -->
+        
+    </div>
+    <!-- /. PAGE INNER  -->
+</div>
 
- <?php include 'footer.php' ?>
 
-   
+
+<?php include 'footer.php' ?>
+
+
